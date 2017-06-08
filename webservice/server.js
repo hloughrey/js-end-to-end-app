8765fe9ad8f0c3eb0cbb640b8177f1b9ac258682
Hugh Loughrey <hugh.loughrey@gmail.com>
2017-06-08 12:24:58 +0100

    Enable CORS

--- webservice/server.js
+++ webservice/server.js
@@ -9,6 +9,13 @@ const routes = require('./routes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Create Routes
 app.use('/', routes);
 
